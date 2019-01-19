3311d3559d912b0bff78c69f1e24f65da9480b0d
Mohammed Algharaballi <malgharaballi@Mohammeds-MacBook-Pro.local>
2019-01-19 12:39:33 +0300

    project complete

--- shop.py
+++ shop.py
@@ -11,8 +11,8 @@ original_price = 2
 signature_price = 2.750
 
 ############################# Start Here! ##############################
-cupcake_shop_name = #complete me!
-signature_flavors = #complete me!
+cupcake_shop_name = "Mogee_Cupcake"
+signature_flavors = ["Pecan pie", "peanut butter", "watermelon"]
 order_list = []
 
 
@@ -20,7 +20,9 @@ def print_menu():
     """
     Print the items in the menu dictionary.
     """
-    # your code goes here!
+    for key,value in menu.items():
+        print(key,value)
+
 
 
 def print_originals():
@@ -28,7 +30,8 @@ def print_originals():
     Print the original flavor cupcakes.
     """
     print("Our original flavor cupcakes (KD %s each):" % original_price)
-    # your code goes here!
+    for cupcake in original_flavors:
+        print (cupcake)
 
 
 def print_signatures():
@@ -36,14 +39,19 @@ def print_signatures():
     Print the signature flavor cupcakes.
     """
     print("Our signature flavor cupcake (KD %s each):" % signature_price)
-    # your code goes here!
+    for cupcake in signature_flavors:
+        print (cupcake)
 
 
 def is_valid_order(order):
-    """
-    Check if an order exists in the shop.
-    """
-    # your code goes here!
+    if order in signature_flavors:
+        return True
+    elif order in original_flavors:
+        return True
+    elif order in menu:
+        return True
+    else:
+        return False
 
 
 def get_order():
@@ -51,8 +59,16 @@ def get_order():
     Repeatedly ask customer for order until they end their order by typing "Exit".
     """
     order_list = []
-    # your code goes here!
-
+    # your code
+    user_input = input("what would you like to order? or Type exit to checkout\n")
+    while user_input.lower()!= "exit":
+        if is_valid_order(user_input) == True:
+             order_list.append(user_input)
+             print ("[%s] has been added to the cart" % user_input)
+             user_input = input("Anything else? \n")
+        else:
+            print("unavailable")
+            user_input = input ("order something else \n")
     return order_list
 
 
@@ -61,14 +77,32 @@ def accept_credit_card(total):
     Return whether an order is eligible for credit card payment.
     """
     # your code goes here!
-
+    if total >= 5:
+        print ("we accept credit card")
+    else:
+        print ("cash only")
 
 def get_total_price(order_list):
     """
     Calculate and return total price of the order.
     """
     total = 0
+    for order in order_list:
+        if order in original_flavors:
+            total += original_price
+        elif order in signature_flavors:
+            total += signature_price
+        elif order in menu:
+            total += menu[order]
+    # for original_flavor in original_flavors:
+    #     if order == original_flavors 
+    #     total += 2
+    # for signature_flavor in signature_flavors:
+    #     if order == signature_flavors
+    #     total += 2.750
+
     # your code goes here!
+
 
     return total
 
@@ -80,3 +114,8 @@ def print_order(order_list):
     print()
     print("Your order is: ")
     # your code goes here!
+    print("this is your final order %s" % order_list)
+    x= get_total_price(order_list)
+    print ("%0.3f KWD" % x)
+
+
